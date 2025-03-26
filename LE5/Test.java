1d258eb8c895a04d761a63805bb3f0e9b9d24d0f
Rithuik Prakash <116014801+Rithui@users.noreply.github.com>
2025-03-26 15:59:31 +0530

    Add files via upload

---
+++ LE5/Test.java
@@ -0,0 +1,8 @@
+class Test{
+       Public static void main(String args[]){
+       Offering offering=new coffee();
+       Offering = new Rum(offering);
+       Offering = new Coffee(offering);
+       System.out.println(offerings.gtName())
+       }
+}
